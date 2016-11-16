d7fe46ca5faee94e2b503203e1e3cd48903ce562
Jordi van Ditmar <jorditmar@hotmail.com>
2016-11-16 21:41:38 +0100

    Verder gewerkt aan alertdialogs, begonnen aan database

--- code/PracticumAssistent/app/src/main/java/nl/mprog/practicumassistent/AddLeerlingDialog.java
+++ code/PracticumAssistent/app/src/main/java/nl/mprog/practicumassistent/AddLeerlingDialog.java
@@ -1,7 +1,11 @@
 package nl.mprog.practicumassistent;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.view.LayoutInflaterCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,8 +14,60 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class AddLeerlingDialog extends DialogFragment implements View.OnClickListener {
+public class AddLeerlingDialog extends DialogFragment{
 
+    public interface DialogListener {
+        public void onDialogPositiveClick(DialogFragment dialog);
+        public void onDialogNegativeClick(DialogFragment dialog);
+    }
+
+    // Use this instance of the interface to deliver action events
+    DialogListener mListener;
+
+    // Override the Fragment.onAttach() method to instantiate the NoticeDialogListener
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            mListener = (DialogListener) activity;
+        } catch (ClassCastException e) {
+            // The activity doesn't implement the interface, throw exception
+            throw new ClassCastException(activity.toString()
+                    + " must implement NoticeDialogListener");
+        }
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        // Get the layout inflater
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_add_leerling, null));
+        builder.setTitle("Nieuwe leerling")
+                // Add action buttons
+                .setPositiveButton("Bevestigen", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getActivity(), "Positive!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Annuleren", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(getActivity(), "Negative!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+        return builder.create();
+    }
+
+
+
+
+    /*
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,4 +91,5 @@ public class AddLeerlingDialog extends DialogFragment implements View.OnClickLis
             Toast.makeText(getActivity(), "OK!", Toast.LENGTH_SHORT).show();
         }
     }
+    */
 }
--- code/PracticumAssistent/app/src/main/java/nl/mprog/practicumassistent/LeerlingenActivity.java
+++ code/PracticumAssistent/app/src/main/java/nl/mprog/practicumassistent/LeerlingenActivity.java
@@ -3,6 +3,7 @@ package nl.mprog.practicumassistent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -10,7 +11,7 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class LeerlingenActivity extends AppCompatActivity {
+public class LeerlingenActivity extends AppCompatActivity implements AddLeerlingDialog.DialogListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +32,26 @@ public class LeerlingenActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                FragmentManager manager = getSupportFragmentManager();
-                AddLeerlingDialog addLeerlingDialog = new AddLeerlingDialog();
-                addLeerlingDialog.show(manager, "AddLeerlingDialog");
+                niuweLeerlingDialog();
             }
         });
+
+    }
+
+    public void niuweLeerlingDialog(){
+        DialogFragment dialog = new AddLeerlingDialog();
+        dialog.show(getSupportFragmentManager(), "AddLeerlingDialog");
+    }
+
+    @Override
+    public void onDialogPositiveClick(DialogFragment dialog) {
+        // User touched the dialog's positive button
+
+    }
+
+    @Override
+    public void onDialogNegativeClick(DialogFragment dialog) {
+        // User touched the dialog's negative button
 
     }
 
